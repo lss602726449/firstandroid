47bf0f1f728f2b48cb12ae991916ab18460742d3
lss602726449 <602726449@qq.com>
2020-02-08 21:59:40 +0800

    加了一个activity

---
+++ app/src/main/java/com/example/first/TestGitActivity.java
@@ -0,0 +1,14 @@
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestGitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_git);
+    }
+}
